e1296a2e9e05e6d1e9d5bfbb6a80dc0421198cd3
rebeccavlee <rebeccavlee@g.ucla.edu>
2026-03-27 13:46:56 -0700

    Update Board.js

--- src/routes/Board.js
+++ src/routes/Board.js
@@ -32,120 +32,129 @@ class Board extends Component {
             <>
                 <BoardProfile width={this.state.width}
                     title="President"
-                    name="Cadence Chang"
-                    major="Computational & Systems Biology, 2025"
-                    img="cadence.jpg"
+                    name="Rebecca Lee"
+                    major="Biochemistry, 2026"
+                    img="RebeccaLee.jpg"
                     size="115% auto"
                     halign="45%"
                     valign="35%"
                 />
                 <BoardProfile width={this.state.width}
                     title="External Vice President"
-                    name="Rebecca Lee"
-                    major="Biochemistry, 2026"
-                    img="rebecca.jpg"
-                    size="180% auto"
+                    name="Shannon Shams"
+                    major="Physiological Sciences, 2026"
+                    img="ShannonShams.jpg"
+                    size="115% auto"
                     halign="50%"
                     valign="20%"
                 />
                 <BoardProfile width={this.state.width}
                     title="Internal Vice President"
-                    name="Luke Jones"
-                    major="Mathematics, 2027"
-                    img="luke.png"
-                    size="115% auto"
-                    halign="45%"
-                    valign="35%"
+                    name="Vacant"
+                    #major=
+                    #img=
+                    #size="115% auto"
+                    #halign="45%"
+                    #valign="35%"
                 />
                 <BoardProfile width={this.state.width}
                     title="Secretary"
-                    name="Joshua Hockman"
-                    major="Biochemistry, 2026"
-                    img="joshua.JPG"
-                    size="232% auto"
+                    name="Scarlett Greenwood"
+                    major="Psychology, 2028"
+                    img="ScarlettGreenwood.jpg"
+                    size="115% auto"
                     halign="47%"
                     valign = "58%"
                 />
                 <BoardProfile width={this.state.width}
                     title="Activities Director"
-                    name="Clarissa Zuo"
-                    major="Physics, 2027"
-                    img="clarissa.jpg"
-                    size="105% auto"
+                    name="Adrian Anaya"
+                    major="Chemical Engineering, 2028"
+                    img="AdrianAnaya.jpg"
+                    size="115% auto"
                     halign="70%"
                     valign="25%"
                 />
                 <BoardProfile width={this.state.width}
                     title="Activities Director"
-                    name="Judy Lee"
-                    major="Psychobiology, 2027"
-                    img="judy.jpg"
-                    size="105% auto"
+                    name="Stephanie Nguyen"
+                    major="Neuroscience, 2028"
+                    img="StephanieNguyen.jpg"
+                    size="115% auto"
                     halign="70%"
                     valign="20%"
                 />
                 <BoardProfile width={this.state.width}
                     title="Community Service Director"
-                    name="Jake Maricich"
-                    major="Neuroscience, 2027"
-                    img="jake.png"
-                    size="110% auto"
+                    name="Izzy Nuñez"
+                    major="Human Biology & Society, 2028"
+                    img="IzzyNunez.png"
+                    size="115% auto"
                     halign = "50%"
                     valign="65%"
                 />
                 <BoardProfile width={this.state.width}
                     title="Educational Programs Director"
-                    name="Shannon Shams"
-                    major="Physiological Science, 2026"
-                    img="shannon.jpg"
-                    size="110% auto"
-                    halign="50%"
-                    valign="60%"
+                    name="Vacant"
+                    #major=
+                    #img=
+                    #size="110% auto"
+                    #halign="50%"
+                    #valign="60%"
                 />
                 <BoardProfile width={this.state.width}
                     title="Funding Director"
-                    name="Chan Park"
-                    major="Mathematics/Economics, 2027"
-                    img="chan.JPG"
-                    size="130% auto"
+                    name="Tomas Mazeika"
+                    major="Philosophy, 2028"
+                    img="TomasMazeika.JPG"
+                    size="115% auto"
                     halign="50%"
                     valign="30%"
                 />
                 <BoardProfile width={this.state.width}
                     title="Treasurer"
-                    name="Kavya Kumar"
-                    major="Economics and Psychology, 2025"
-                    img="kavya.png"
-                    size="130% auto"
-                    halign="50%"
-                    valign="30%"
+                    name="Vacant"
+                    #major=
+                    #img=
+                    #size="130% auto"
+                    #halign="50%"
+                    #valign="30%"
                 />
                 <BoardProfile width={this.state.width}
                     title="Publicity Director"
                     name="Katie Fang"
                     major="World Arts and Cultures, 2026"
-                    img="katie.jpg"
+                    img="KatieFang.jpg"
                     size="170% auto"
                     halign="55%"
                     valign="38%"
                 />
                 <BoardProfile width={this.state.width}
                     title="Transfer Affairs Director"
-                    name="Evette Palacios"
-                    major="Political Science and Education, 2025"
-                    img="evette.jpg"
-                    size="100% auto"
+                    name="Carlee Lincoln"
+                    major="Political Science and Philosophy, 2026"
+                    img="CarleeLincoln.jpg"
+                    size="115% auto"
                     halign="50%"
                     valign="20%"
                 />
+                        <BoardProfile width={this.state.width}
+                    title="Mentorship Director"
+                    name="Téa Wagstaff"
+                    major="Mathematics/Economics, 2027"
+                    img="TeaWagstaff.jpg"
+                    #size="115% auto"
+                    #halign="55%"
+                    #valign="58%"
+                />
                 <BoardProfile width={this.state.width}
                     title="Webmaster"
-                    name="Nyla Zia"
-                    major="Computer Science, 2026"
-                    img="nyla.jpg"
-                    size="100% auto"
-                    halign="55%"
-                    valign="58%"
+                    name="Vacant"
+                    #major=
+                    #img=
+                    #size="100% auto"
+                    #halign="55%"
+                    #valign="58%"
                 />
             </>
         )
